eba321938520e3ac9df0fa68e42c1f26a17d8f2c
alekzonder <alekzonder@gmail.com>
2019-12-17 13:55:02 +0700

    chg: add build script

---
+++ web_page_replay_go/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# https://github.com/mitchellh/gox
+gox -osarch="darwin/amd64" -osarch="linux/amd64" ./src
+
+
+# darwin
+rm -rf ./wpr
+mkdir -p ./wpr
+mv src_darwin_amd64 ./wpr/wpr
+cp wpr_cert.pem ./wpr/
+cp wpr_key.pem ./wpr/
+cp deterministic.js ./wpr/
+
+echo 'build and copied from catapult project for Macos (darwin)' > ./wpr/README.md
+echo 'see https://github.com/alekzonder/catapult/tree/custom/web_page_replay_go' >> ./wpr/README.md
+
+tar czf ./wpr-darwin.tgz ./wpr
+
+
+# linux
+
+rm -rf ./wpr
+mkdir -p ./wpr
+mv src_linux_amd64 ./wpr/wpr
+cp wpr_cert.pem ./wpr/
+cp wpr_key.pem ./wpr/
+cp deterministic.js ./wpr/
+
+echo 'build and copied from catapult project for linux (linux)' > ./wpr/README.md
+echo 'see https://github.com/alekzonder/catapult/tree/custom/web_page_replay_go' >> ./wpr/README.md
+
+tar czf ./wpr-linux.tgz ./wpr
